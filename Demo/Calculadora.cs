c43aff4428d8198b5c7772939b40254258f941b5
Marcelo Amador <marcelo@cklabs.dev>
2023-10-29 13:17:53 -0300

    Projeto para testes finalizado

---
+++ Demo/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double a, double b)
+        {
+            return a + b;
+        }
+
+        public int Dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
